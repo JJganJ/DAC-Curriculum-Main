88a8ec7c1768b9cbc5b5531bc57ce3e8e2293671
jjganj <jjjgan001@mymail.sim.edu.sg>
2024-11-07 21:55:55 +0800

    cool

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=c:\Users\Jason Gan\Desktop\DAC-Curriculum-Main\.venv
+set VIRTUAL_ENV=C:\Users\Jason Gan\Desktop\DAC-Curriculum-Main\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
